96fc89b7e048d0b6815d4a8169236eed59ee3de1
Tomislav <trba8l@live.com>
2024-10-10 13:57:15 +0200

    added login form to react

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
+import Login from "./components/Login.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </Router>
   </React.StrictMode>
