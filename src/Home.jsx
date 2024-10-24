66ac38e0594b5451e1ec64b2dc69191b73333109
Tomislav <trba8l@live.com>
2024-10-24 13:24:15 +0200

    improve and add component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,52 +1,60 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-function Home() {
-  const [projects, setProjects] = useState(null);
+const Home = () => {
+  // const [projects, setProjects] = useState(null);
 
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:8000/projects');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setProjects(data);
-      } catch (error) {
-        console.error('Error fetching projects:', error);
+  // useEffect(() => {
+  //   const fetchProjects = async () => {
+  //     try {
+  //       const response = await fetch('http://127.0.0.1:8000/projects');
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       const data = await response.json();
+  //       setProjects(data);
+  //     } catch (error) {
+  //       console.error('Error fetching projects:', error);
         
-      }
-    };
+  //     }
+  //   };
 
-    fetchProjects();
-  }, []);
+  //   fetchProjects();
+  // }, []);
 
-  if (projects === null) {
-    return <div>Loading...</div>;
-  }
+  // if (projects === null) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
-    <div className="overflow-x-auto">
-      <table className="min-w-full bg-white border-collapse">
-        <thead>
-          <tr className="bg-gray-50 uppercase text-sm leading-normal text-center">
-            <th className="py-3 px-6 text-left">Key</th>
-            <th className="py-3 px-6 text-left">Value</th>
-          </tr>
-        </thead>
-        <tbody className="text-md font-sans leading-6 text-left text-gray-900">
-          {Object.entries(projects).map(([key, value]) => (
-            <tr key={key} className="border-b border-gray-200 hover:bg-gray-100">
-              
-              <td className="py-3 px-6 whitespace-nowrap">
-                {typeof value === 'object' ? JSON.stringify(value, null, 2) : value}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <h1>Homepage with JWT Auth</h1>
+    <ul>
+      <li><Link to={`Login`}>Login</Link></li>
+      <li><Link to={`SignUp`}>SignUp</Link></li>
+    </ul>
     </div>
+    // <div className="overflow-x-auto">
+    //   <table className="min-w-full bg-white border-collapse">
+    //     <thead>
+    //       <tr className="bg-gray-50 uppercase text-sm leading-normal text-center">
+    //         <th className="py-3 px-6 text-left">Key</th>
+    //         <th className="py-3 px-6 text-left">Value</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody className="text-md font-sans leading-6 text-left text-gray-900">
+    //       {Object.entries(projects).map(([key, value]) => (
+    //         <tr key={key} className="border-b border-gray-200 hover:bg-gray-100">
+              
+    //           <td className="py-3 px-6 whitespace-nowrap">
+    //             {typeof value === 'object' ? JSON.stringify(value, null, 2) : value}
+    //           </td>
+    //         </tr>
+    //       ))}
+    //     </tbody>
+    //   </table>
+    // </div>
   );
 
   
