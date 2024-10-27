998a2f57adc4ed9a029cfe470dfcb76e9c443121
Tomislav <trba8l@live.com>
2024-10-27 10:14:39 +0100

    make form for sign up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default SignUp
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1>SignUp</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="passord"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
